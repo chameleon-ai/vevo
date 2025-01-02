11f0e1ea5ef67f35e1dddd8f91ad7c2aefd8a0f0
chameleon-ai <ChamelAIon@proton.me>
2025-01-02 10:58:20 -0600

    Resolved initial dependencies

---
+++ app.py
@@ -0,0 +1,7 @@
+import os
+import sys
+sys.path.append(os.getcwd() + '/Amphion') # For importing modules relative to the Amphion directory
+from Amphion.models.vc.vevo.vevo_utils import *
+
+if __name__ == '__main__':
+    print('yolo')
